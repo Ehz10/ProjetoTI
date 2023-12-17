0c203682979e555aa25de0c0d3638cb26e89f592
Guerreir02 <jfsguerreiro1@gmail.com>
2023-12-17 23:10:54 +0000

    Merge branch 'main' of https://github.com/Ehz10/ProjetoTI

---
+++ script.js
@@ -0,0 +1,9 @@
+$('input').on('change', function() {
+    $('body').toggleClass('blue');
+  });
+
+//Qunado abrir o site colocar o volume a 15%
+document.addEventListener('DOMContentLoaded', function () {
+    var audio = document.getElementById('audio')
+  audio.volume = 0.15;
+});
