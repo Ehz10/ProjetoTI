7cca6c9df055beea3714f791d0b16746c0ac4cd9
Guerreir02 <jfsguerreiro1@gmail.com>
2023-12-17 23:23:25 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+//Quando abrir o site colocar o volume a 15%
+document.addEventListener('DOMContentLoaded', function () {
+  var audio = document.getElementById('audio')
+audio.volume = 0.15;
+});
+
 $('input').on('change', function() {
     $('body').toggleClass('blue');
   });
 
-//Quando abrir o site colocar o volume a 15%
-document.addEventListener('DOMContentLoaded', function () {
-    var audio = document.getElementById('audio')
-  audio.volume = 0.15;
-});
+
 
